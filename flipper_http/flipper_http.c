b3012084f09857dc4d8c9f75f605af0edb3c7dd0
jblanked <82678820+jblanked@users.noreply.github.com>
2024-11-15 11:31:29 -0500

    Update flipper_http.c

--- flipper_http/flipper_http.c
+++ flipper_http/flipper_http.c
@@ -180,9 +180,11 @@ int32_t flipper_http_worker(void *context)
                     // Write to file if buffer is full
                     if (file_buffer_len >= FILE_BUFFER_SIZE)
                     {
-                        // remove [POST/END] and/or [GET/END] from the file
                         if (!flipper_http_append_to_file(
-                                file_buffer, file_buffer_len, false, fhttp.file_path))
+                                file_buffer,
+                                file_buffer_len,
+                                !fhttp.just_started_get && !fhttp.just_started_post,
+                                fhttp.file_path))
                         {
                             FURI_LOG_E(HTTP_TAG, "Failed to append data to file");
                         }
